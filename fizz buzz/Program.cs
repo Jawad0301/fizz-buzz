efb507c634430f4b49933eaec100344e2373b25a
jaal0301 <jaal0301@S26513>
2024-02-01 14:16:21 +0100

    Add project files.

---
+++ fizz buzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizz_buzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i =0; i <100; i++) 
+            {
+                if (i % 15== 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5== 0) 
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3== 0) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 2!= 0) 
+                {
+                    Console.WriteLine("Unven" + i . ToString());
+                }
+                else 
+                {
+                      Console.WriteLine(i);
+                }
+                  i=i+1;
+            }
+            
+        }
+    }
+}
